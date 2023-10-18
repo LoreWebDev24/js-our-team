2b1264f14d372a6a1ca336c18031c56ae0eb0fa8
LoreWebDev24 <lorenzo.delre12@gmail.com>
2023-10-18 15:35:52 +0200

    add console log of the object

--- js/master.js
+++ js/master.js
@@ -11,4 +11,6 @@ const team = {
     "foto" : ['wayne-barnett-founder-ceo.jpg','angela-caroll-chief-editor.jpg','walter-gordon-office-manager.jpg','angela-lopez-social-media-manager.jpg','scott-estrada-developer.jpg','barbara-ramos-graphic-designer.jpg'],
 }
 
+console.log(team)
+
 
